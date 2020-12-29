3e6216be0768b6686bae0f7fce94162b4176f8f9
Venkat Ramaraju <venky2063@gmail.com>
2020-12-29 10:53:25 -0800

    Removed repetition, shared functions across files

--- Data_Collection/conversations_scraper.py
+++ Data_Collection/conversations_scraper.py
@@ -1,66 +1,25 @@
 # Libraries and Dependencies
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import numpy as np
-
-
-def cleanup(line):
-    cleaned_text = str.maketrans('', '', r"-()\"#/@;:<>{}-=~|.?,")
-    line = line.translate(cleaned_text)
-
-    return str(line).lower()
-
-
-def get_soup(request, element, class_value):
-    html_page = requests.get(request).text
-    soup = BeautifulSoup(html_page, 'lxml')
-    return soup.find_all(element, class_=class_value)
-
-
-def create_conversation_array(conversation_list):
-    list_of_conversations = []
-    for li in conversation_list:
-        list_of_conversations.append(li.text)  # Gets the text from HTML
-
-    return list_of_conversations
+from headlines_scraper import *
 
 
 def get_yahoo_conversations(stock):
     request = 'https://finance.yahoo.com/quote/' + stock + '/community?p=' + stock
     opinions = get_soup(request, 'div', 'C($c-fuji-grey-l) Mb(2px) Fz(14px) Lh(20px) Pend(8px)')
-    return create_conversation_array(opinions)
-
-
-def format_to_table(conversations, stock):
-    title = 'Recent conversations for ' + stock
-    opinion_table = pd.DataFrame(columns=[title])
-
-    for line in conversations:
-        opinion_table = opinion_table.append({title: cleanup(line)}, ignore_index=True)
-
-    return opinion_table
+    return create_array(opinions)
 
 
 def main():
     # Stock Ticker
-    stock = 'NFLX'
+    stock = 'TSLA'
     print("\nFetching conversations for " + stock + "...\n")
-    title = 'Recent conversations for ' + stock
+    title = 'Recent headlines/conversations for ' + stock
 
     # List of sources
     source_1 = np.array(get_yahoo_conversations(stock))
+
+    # Combining all sources, initializing data frame
     overall_conversations = list(np.concatenate(source_1, axis=None))
-    opinion_table = pd.DataFrame(columns=[title])
-
-    # Formatting to a table
-    if len(source_1) != 0:
-        opinion_table = format_to_table(overall_conversations, stock)
-    else:
-        print("Invalid ticker or no conversations available.")
-
-    opinion_table.drop_duplicates(subset=title, keep=False, inplace=True)
-    print(opinion_table)
+    output(overall_conversations, stock)
 
 
 if __name__ == "__main__":
--- Data_Collection/headlines_scraper.py
+++ Data_Collection/headlines_scraper.py
@@ -16,17 +16,17 @@ def get_soup(request, element, class_value):
     return soup.find_all(element, class_=class_value)
 
 
-def create_headlines_array(headlines_list):
-    list_of_headlines = []
-    for li in headlines_list:
+def create_array(data_list):
+    result_array = []
+    for li in data_list:
         if li.text != "":
-            list_of_headlines.append(' '.join(li.text.split()))  # Removes tabs, newlines, and gets text from HTML
+            result_array.append(' '.join(li.text.split()))  # Removes tabs, newlines, and gets text from HTML
 
-    return list_of_headlines
+    return result_array
 
 
 def format_to_table(headlines, stock):
-    title = 'Recent headlines for ' + stock
+    title = 'Recent headlines/conversations for ' + stock
     headlines_table = pd.DataFrame(columns=[title])
 
     for line in headlines:
@@ -38,19 +38,19 @@ def format_to_table(headlines, stock):
 def get_morningstar_headlines(stock):
     request = 'https://www.morningstar.com/stocks/xnas/' + stock.lower() + '/news'
     headlines_list = get_soup(request, 'a', 'mdc-link mdc-news-module__headline mds-link mds-link--no-underline')
-    return create_headlines_array(headlines_list)
+    return create_array(headlines_list)
 
 
 def get_usa_today_headlines(stock):
     request = 'https://www.usatoday.com/search/?q=' + stock
     headlines_list = get_soup(request, 'a', 'gnt_se_a gnt_se_a__hd gnt_se_a__hi')
-    return create_headlines_array(headlines_list)
+    return create_array(headlines_list)
 
 
 def get_reuters_headlines(stock):
     request = 'https://www.reuters.com/search/news?blob=' + stock
     headlines_list = get_soup(request, 'h3', 'search-result-title')
-    return create_headlines_array(headlines_list)
+    return create_array(headlines_list)
 
 
 def get_cnbc_headlines(stock):
@@ -66,10 +66,24 @@ def get_cnbc_headlines(stock):
     return list_of_headlines
 
 
+def output(overall_data, stock):
+    title = 'Recent headlines/conversations for ' + stock
+    headlines_table = pd.DataFrame(columns=[title])
+
+    # Formatting to a table
+    if len(overall_data) != 0:
+        headlines_table = format_to_table(overall_data, stock)
+    else:
+        print("Invalid ticker or no headlines/conversations available.")
+
+    # Printing out formatted table of headlines
+    headlines_table.drop_duplicates(subset=title, keep=False, inplace=True)
+    print(headlines_table)
+
+
 def main():
     # Stock Ticker
     stock = 'AAPL'
-    title = 'Recent headlines for ' + stock
     print("\nFetching headlines for " + stock + "...\n")
 
     # List of sources
@@ -79,19 +93,10 @@ def main():
     source_4 = np.array(get_usa_today_headlines(stock))
 
     # Combining all sources, initializing data frame
-    overall_headlines = np.concatenate((source_1, source_2, source_3, source_4), axis=None)
-    overall_headlines = list(overall_headlines)
-    headlines_table = pd.DataFrame(columns=[title])
+    overall_headlines = list(np.concatenate((source_1, source_2, source_3, source_4), axis=None))
 
-    # Formatting to a table
-    if len(overall_headlines) != 0:
-        headlines_table = format_to_table(overall_headlines, stock)
-    else:
-        print("Invalid ticker or no headlines available.")
-
-    # Printing out formatted table of headlines
-    headlines_table.drop_duplicates(subset=title, keep=False, inplace=True)
-    print(headlines_table)
+    # Output
+    output(overall_headlines, stock)
 
 
 if __name__ == "__main__":
