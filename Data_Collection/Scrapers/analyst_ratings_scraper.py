cb21bffe6c01640807add9510292b31374f23f40
Venkat Ramaraju <venky2063@gmail.com>
2021-01-24 14:31:46 -0700

    Added twitter ratings

--- Data_Collection/Scrapers/analyst_ratings_scraper.py
+++ Data_Collection/Scrapers/analyst_ratings_scraper.py
@@ -25,7 +25,7 @@ def get_analyst_ratings(stock):
 
 def main():
     # Tickers and companies
-    stocks = ["TSLA", "NFLX", "AAPL"]
+    stocks = ["TSLA", "NFLX", "AAPL", "TWTR"]
 
     for stock in stocks:
         get_analyst_ratings(stock)
