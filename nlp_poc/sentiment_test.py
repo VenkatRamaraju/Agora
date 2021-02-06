c0b078e2fdae5781edef584afebb8e67075de6e4
Venkat Ramaraju <venky2063@gmail.com>
2021-02-06 10:16:56 -0700

    Updated polarities, analysis on accuracy remains

--- nlp_poc/sentiment_test.py
+++ nlp_poc/sentiment_test.py
@@ -4,11 +4,9 @@ import os
 
 sia = SentimentIntensityAnalyzer()
 
-file_path = "../Thesis-Source-Code/Data_Collection/CSV_Results/"
+file_path = "../Data_Collection/CSV_Results/"
 
-csvs = [f for f in os.listdir("../Thesis-Source-Code/Data_Collection/CSV_Results/") if f.endswith("csv")]
-
-print(csvs)
+csvs = [f for f in os.listdir("../Data_Collection/CSV_Results/") if f.endswith("csv")]
 
 for csv in csvs:
     csv_df = pd.read_csv(file_path + csv)
@@ -19,18 +17,14 @@ for csv in csvs:
         scores = sia.polarity_scores(text)  # print 'scores' object to see all of the fields offered
         row["polarity"] = scores["compound"]  # compound field shows a holistic view of the derived sentiment
 
-    print(csv_df.head)
-
     file_name = csv.split(".")[0] + "_+_polarity"
 
-    csv_df.to_csv(f"../Thesis-Source-Code/nlp_poc/csvs_with_polarity/{file_name}.csv")
+    csv_df.to_csv(f"../nlp_poc/csvs_with_polarity/{file_name}.csv")
 
 ##############################################################
 # TODO:
-#   - Polish the scrapers to include punctuation and capitalization
 #   - Work on getting the sentiment in a better way
 #   - Start looking at what a weighted average would look like
-#   - Determine a way to factor in the Analyst Ratings data as well
 ##############################################################
 
 
