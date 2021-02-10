5b5606a2820d75ef2057e7c2a6b303e9cb5c0b23
Venkat Ramaraju <venky2063@gmail.com>
2021-02-10 09:51:22 -0700

    Format analysis

--- nlp_poc/sentiment_test.py
+++ nlp_poc/sentiment_test.py
@@ -15,18 +15,14 @@ def get_sentiments():
     file_path = "../Data_Collection/CSV_Results/"
     
     all_csv_results = [f for f in os.listdir("../Data_Collection/CSV_Results/") if f.endswith("csv")]
-    
+
+    print("Analysis:\n")
     for csv in all_csv_results:
         csv_df = pd.read_csv(file_path + csv)
-        csv_df['Negative'] = ""
-        csv_df['Positive'] = ""
-        csv_df['Neutral'] = ""
-        csv_df["Compound"] = ""
+        csv_df["Compound"], csv_df['Neutral'], csv_df['Negative'], csv_df['Positive'] = "", "", "", ""
         avg = 0.0
         rows = 0
-        positive = 0
-        zero = 0
-        negative = 0
+        zero, positive, negative = 0, 0, 0
 
         for index, row in csv_df.iterrows():
             text = row[csv_df.columns[0]]
@@ -51,10 +47,10 @@ def get_sentiments():
 
         # Analysis
         print(csv.split(".")[0].split("_")[0], csv.split(".")[0].split("_")[1])
-        print("Average: ", avg/rows)
-        print("Positive: ", positive)
-        print("Negative: ", negative)
-        print("Zero: ", zero)
+        print("Average Sentiment: ", round(avg/rows, 3))
+        print("Positive : ", round((positive/rows)*100, 2), "%")
+        print("Negative : ", round((negative/rows)*100, 2), "%")
+        print("Zero: ", round((zero/rows)*100, 2), "%")
         print()
 
 
