562bcdcf164b3cdb493b335356343f90a7be910c
Venkat Ramaraju <venky2063@gmail.com>
2021-08-17 15:28:05 -0700

    Added some docs

--- Data_Collection/Scrapers/analyst_ratings_scraper.py
+++ Data_Collection/Scrapers/analyst_ratings_scraper.py
@@ -11,11 +11,17 @@ import pandas as pd
 
 
 def analyst_ratings_scraper(stock):
+    """
+    Parses Benzinga's analyst ratings and returns the highest voted analyst rating
+    """
     analyst_ratings = pd.read_html('https://www.benzinga.com/stock/' + stock.lower() + '/ratings')
     return analyst_ratings[0]['Current'].value_counts().idxmax()
 
 
 def build_analyst_csv():
+    """
+    Builds the dataframe for all stock tickers in companies.csv
+    """
     stocks_df = pd.read_csv("../companies.csv")
     stocks_dict = {}
 
@@ -26,6 +32,7 @@ def build_analyst_csv():
 
     stocks = list(stocks_dict.keys())
 
+    # Output to csv
     df = pd.DataFrame(columns=['Ticker', 'Rating'])
     for stock in stocks:
         df = df.append({'Ticker': stock, 'Rating': analyst_ratings_scraper(stock)}, ignore_index=True)
