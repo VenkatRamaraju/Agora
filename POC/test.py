6df4a4169ac7c86c35d73a3379c132345c4ac8a6
Venkat Ramaraju <venky2063@gmail.com>
2021-09-18 10:49:18 -0700

    ok

--- POC/test.py
+++ POC/test.py
@@ -17,26 +17,74 @@ def get_data():
     files = [f for f in os.listdir('Stocks') if f.endswith('.csv')]
     master_dict = {}
     company_dict = {}
+    analyst_dict = {}
+    training_df = pd.DataFrame(columns=['Symbol', 'Name', 'Buy'])
 
     for file in files:
-        if file == 'nyse_buy.csv' or file == 'nasdaq_buy.csv':
+        if file == 'nasdaq_strong_buy.csv' or file == 'nyse_strong_buy.csv':
             df = pd.read_csv('Stocks/' + str(file))
             for index, row in df.iterrows():
                 master_dict[row['Symbol']] = 1
+                analyst_dict[row['Symbol']] = "Strong Buy"
                 company_dict[row['Symbol']] = row['Name']
-        else:
+        elif file == 'nasdaq_buy.csv' or file == 'nyse_buy.csv':
+            df = pd.read_csv('Stocks/' + str(file))
+            for index, row in df.iterrows():
+                master_dict[row['Symbol']] = 1
+                analyst_dict[row['Symbol']] = "Buy"
+                company_dict[row['Symbol']] = row['Name']
+        elif file == 'nasdaq_hold.csv' or file == 'nyse_hold.csv':
             df = pd.read_csv('Stocks/' + str(file))
             for index, row in df.iterrows():
                 master_dict[row['Symbol']] = 0
+                analyst_dict[row['Symbol']] = "Hold"
                 company_dict[row['Symbol']] = row['Name']
 
-    master_df = pd.DataFrame(columns=['Symbol', 'Name', 'Buy'])
+                training_row = {'Symbol': row['Symbol'], 'Name': company_dict[row['Symbol']],
+                                'Buy': master_dict[row['Symbol']], 'Analyst': analyst_dict[row['Symbol']]}
+                training_df = training_df.append(training_row, ignore_index=True)
+
+
+        elif file == 'nasdaq_sell.csv' or file == 'nyse__sell.csv':
+            df = pd.read_csv('Stocks/' + str(file))
+            for index, row in df.iterrows():
+                master_dict[row['Symbol']] = 0
+                analyst_dict[row['Symbol']] = "Sell"
+                company_dict[row['Symbol']] = row['Name']
+
+                training_row = {'Symbol': row['Symbol'], 'Name': company_dict[row['Symbol']],
+                                'Buy': master_dict[row['Symbol']], 'Analyst': analyst_dict[row['Symbol']]}
+                training_df = training_df.append(training_row, ignore_index=True)
+
+
+        elif file == 'nasdaq_strong_sell.csv' or file == 'nyse_strong_sell.csv':
+            df = pd.read_csv('Stocks/' + str(file))
+            for index, row in df.iterrows():
+                master_dict[row['Symbol']] = 0
+                analyst_dict[row['Symbol']] = "Strong Sell"
+                company_dict[row['Symbol']] = row['Name']
+
+                training_row = {'Symbol': row['Symbol'], 'Name': company_dict[row['Symbol']],
+                                'Buy': master_dict[row['Symbol']], 'Analyst': analyst_dict[row['Symbol']]}
+                training_df = training_df.append(training_row, ignore_index=True)
+
+    master_df = pd.DataFrame(columns=['Symbol', 'Name', 'Buy', 'Analyst'])
 
     for ticker in master_dict:
-        ticker_row = {'Symbol': ticker, 'Name': company_dict[ticker], 'Buy': master_dict[ticker]}
-        master_df = master_df.append(ticker_row, ignore_index=True)
+        db_row = {'Symbol': ticker, 'Name': company_dict[ticker], 'Buy': master_dict[ticker],
+                  'Analyst': analyst_dict[ticker]}
+        master_df = master_df.append(db_row, ignore_index=True)
 
-    master_df.to_csv('recent_training_data.csv')
+    russell_df = pd.read_csv('Russell.csv')
+    for index, row in russell_df.iterrows():
+        if row['Symbol'] in master_dict:
+            if analyst_dict[row['Symbol']] is 'Buy' or analyst_dict[row['Symbol']] is 'Strong Buy':
+                training_row = {'Symbol': row['Symbol'], 'Name': company_dict[row['Symbol']], 'Buy': master_dict[row['Symbol']],
+                      'Analyst': analyst_dict[row['Symbol']]}
+                training_df = training_df.append(training_row, ignore_index=True)
+
+    master_df.to_csv('final_db.csv')
+    training_df.to_csv('companies.csv')
 
 
 def main():
