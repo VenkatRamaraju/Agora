702310bdfa3f089564502ae50b0b7da3ee75593b
Venkat Ramaraju <venky2063@gmail.com>
2021-09-18 19:24:08 -0700

    training df

--- Model_Training/model.py
+++ Model_Training/model.py
@@ -28,6 +28,7 @@ def get_data():
 
     for index, row in df.iterrows():
         ticker = yf.Ticker(row['Ticker'])
+        print(ticker)
         if ticker is not None:
             for col in new_columns:
                 if col in ticker.info:
@@ -36,8 +37,24 @@ def get_data():
     df.to_csv('final_training_data.csv')
 
 
+def add_analyst_rating():
+    buy_dict = {}
+    df = pd.read_csv('../Data_Collection/companies.csv', index_col=0)
+    for index, row in df.iterrows():
+        buy_dict[row['Symbol']] = row['Buy']
+
+    training_df = pd.read_csv('final_training_data.csv')
+    training_df['Buy'] = -1
+
+    for index, row in training_df.iterrows():
+        training_df.at[index, 'Buy'] = buy_dict[row['Ticker']]
+
+    training_df.to_csv('overall_training_data.csv', index=False)
+
+
 def main():
     get_data()
+    add_analyst_rating()
 
 
 if __name__ == "__main__":
