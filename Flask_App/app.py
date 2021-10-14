dea715df29e09a0089e2fb6e257a94dae56f23c2
Jayanth Rao <jrao1724@gmail.com>
2021-10-13 20:16:23 -0700

    ui is working

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import sqlite3
 from sqlite3 import Error
 import os.path
+from fetch_top_stocks_yf import get_trending_stocks
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 print(BASE_DIR)
@@ -24,40 +25,70 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    trending_stocks = get_trending_stocks()
+    conn = get_db_connection()
+    all_tickers_sql = conn.execute(
+        "select ticker from TickerStockMetrics;"
+    ).fetchall()
+
+    all_tickers = []
+    for i in all_tickers_sql:
+        all_tickers.append(i[0])
+
+    trending_tickers = []
+    for i in trending_stocks:
+        trending_tickers.append(i['ticker'])
+
+    filtered_tickers = [x for x in trending_tickers if x in all_tickers]
+
+    return render_template('index.html',
+                           trending_stocks=filtered_tickers[:5])
 
 
-@app.route('/data', methods=['POST', 'GET'])
+@app.route('/data/', methods=['GET', 'POST'])
 def data():
+    conn = get_db_connection()
+    ticker_name = ""
     if request.method == 'GET':
-        return f"The URL '/data' is accessed directly"
+        # return f"The URL '/data' is accessed directly"
+        ticker_name = request.args["Searched Ticker"]
     if request.method == 'POST':
         form_data = request.form
-        conn = get_db_connection()
         ticker_name = form_data["Searched Ticker"]
 
-        predictions_sql = f"""
-                Select * from TickerPredictions where ticker="{ticker_name}";
+    all_tickers_sql = conn.execute(
+        "select ticker from TickerStockMetrics;"
+    ).fetchall()
+
+    all_tickers = []
+    for i in all_tickers_sql:
+        all_tickers.append(i[0])
+
+    if ticker_name not in all_tickers:
+        return redirect("/")
+
+    predictions_sql = f"""
+            Select * from TickerPredictions where ticker="{ticker_name}";
+            """
+    ticker_preds = conn.execute(predictions_sql).fetchall()
+
+    info_sql = f"""
+          Select * from TickerStockMetrics where ticker="{ticker_name}";
+          """
+    ticker_info = conn.execute(info_sql).fetchall()
+
+    if ticker_info == [] or ticker_preds == []:     # Enter a valid ticker
+        return render_template('index.html')
+
+    headlines_sql = f"""
+                Select headlines from TickerHeadlines where ticker="{ticker_name}" limit 3
                 """
-        ticker_preds = conn.execute(predictions_sql).fetchall()
+    ticker_headlines = conn.execute(headlines_sql).fetchall()
 
-        info_sql = f"""
-              Select * from TickerStockMetrics where ticker="{ticker_name}";
-              """
-        ticker_info = conn.execute(info_sql).fetchall()
+    conn.close()
 
-        if ticker_info == [] or ticker_preds == []:     # Enter a valid ticker
-            return render_template('index.html')
-
-        headlines_sql = f"""
-                    Select headlines from TickerHeadlines where ticker="{ticker_name}" limit 3
-                    """
-        ticker_headlines = conn.execute(headlines_sql).fetchall()
-
-        conn.close()
-
-        return render_template('data.html',
-                               form_data=form_data,
-                               ticker_info=ticker_info,
-                               ticker_headlines=ticker_headlines,
-                               ticker_predictions=ticker_preds)
+    return render_template('data.html',
+                           ticker_name=ticker_name,
+                           ticker_info=ticker_info,
+                           ticker_headlines=ticker_headlines,
+                           ticker_predictions=ticker_preds)
