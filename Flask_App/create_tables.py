d0fcc6697870f2fb29268baeaaa7bd1220ba5c60
Venkat Ramaraju <venky2063@gmail.com>
2021-10-30 19:42:45 -0700

    refreshed entire dataset

--- Flask_App/create_tables.py
+++ Flask_App/create_tables.py
@@ -114,16 +114,6 @@ def create_stock_metrics_table(connection, cur):
     new_ticker_info.columns = ['ticker', 'beta', 'profit_margins', 'forward_eps', 'book_value',
                                'held_percent_institutions', 'short_ratio', 'short_percent_of_float']
 
-    # new_ticker_info = new_ticker_info.round({
-    #     'beta': 2,
-    #     'profit_margins': 2,
-    #     'forward_eps': 2,
-    #     'book_value': 2,
-    #     'held_percent_institutions': 2,
-    #     'short_ratio': 2,
-    #     'short_percent_of_float': 2
-    # })
-
     # Insert data into table
     new_ticker_info.to_sql(table1, connection, if_exists="append", index=False)
 
